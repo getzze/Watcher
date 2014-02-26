20e93ba9b5da25d8a371209ca9109479e4c80933
getzze <getzze@gmail.com>
2014-02-26 12:27:34 +0000

    Adapt watcher.py to use with subliminal
    Add exclude_extensions and include_extensions to watched directory

--- watcher.py
+++ watcher.py
@@ -3,7 +3,8 @@
 
 ##/usr/bin/env python
 
-# Copyright (c) 2014 Bertrand Lacoste
+# Modified by Bertrand Lacoste
+# Copyright (c) 2010 Greggory Hernandez
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
